9e99bf44f103f3cbada65a6e41c1281e4922e9cc
Parv Gupta <Parvg555@gmail.com>
2023-02-24 21:10:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+let input1 = prompt("Enter the first number:");
+let input2 = prompt("Enter the second number:");
+
+// Check that the input is numerical
+if (input1 === "" || input2 === "" || isNaN(input1) || isNaN(input2)) {
+  alert("Invalid input. Please enter a valid number.");
+} else {
+  // Add the two input numbers
+  let sum = Number(input1) + Number(input2);
+
+  // Display the sum to the user
+  alert(`The sum of ${input1} and ${input2} is ${sum}.`);
+}
